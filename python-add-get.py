fff3d7665bb40fef9f0e5f98daf816872c0ce5cb
Harjinder Dhanjal <malotian@gmail.com>
2019-11-27 15:31:06 +0000

    refactorization

--- python-add-get.py
+++ python-add-get.py
@@ -3,15 +3,6 @@ from flask import request, jsonify
 
 
 def main():
-    query_parameters = request.args
-    if 'number1' in request.args:
-        number1 = int(request.args.get('number1'))
-    else:
-        return 'Error: No number1 field provided. Please specify an number1.'
-
-    if 'number2' in request.args:
-        number2 = int(request.args.get('number2'))
-    else:
-        return 'Error: No number2 field provided. Please specify an number2.'
-
+    number1 = int(request.args.get('number1'))
+    number2 = int(request.args.get('number2'))
     return (jsonify({'total': number1 + number2}), 200)
